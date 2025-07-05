6d9e16064488e3e0dc7f3c9dfd6ef1c0d9819408
mqole <anactualpanacea@gmail.com>
2025-07-06 02:10:07 +1000

    merge conflict test

--- Content.Server/Containers/EmptyOnMachineDeconstructComponent.cs
+++ Content.Server/Containers/EmptyOnMachineDeconstructComponent.cs
@@ -1,7 +1,7 @@
 namespace Content.Server.Containers
 {
     /// <summary>
-    /// Empties a list of containers when the machine is deconstructed via MachineDeconstructedEvent.
+    /// Empties a list of containers when the machine is deconstructed via MachineDeconstructedEvent. // my cat is so loud dog
     /// </summary>
     [RegisterComponent]
     public sealed partial class EmptyOnMachineDeconstructComponent : Component
