9542882f28d9981c1b039da7cb09439cd88d7072
mqole <anactualpanacea@gmail.com>
2025-07-06 02:07:50 +1000

    and upstream test Once More

--- Content.Server/Containers/EmptyOnMachineDeconstructComponent.cs
+++ Content.Server/Containers/EmptyOnMachineDeconstructComponent.cs
@@ -1,7 +1,7 @@
 namespace Content.Server.Containers
 {
     /// <summary>
-    /// Empties a list of containers when the machine is deconstructed via MachineDeconstructedEvent.
+    /// Empties a list of containers when the machine is deconstructed via MachineDeconstructedEvent. PENIS
     /// </summary>
     [RegisterComponent]
     public sealed partial class EmptyOnMachineDeconstructComponent : Component
--- Content.Server/_Goobstation/Changeling/ChangelingEggSystem.cs
+++ Content.Server/_Goobstation/Changeling/ChangelingEggSystem.cs
@@ -3,7 +3,7 @@ using Content.Shared.Changeling;
 using Content.Shared.Mind;
 using Robust.Shared.Timing;
 
-namespace Content.Server.Changeling; // weewoo
+namespace Content.Server.Changeling;
 
 public sealed partial class ChangelingEggSystem : EntitySystem
 {
