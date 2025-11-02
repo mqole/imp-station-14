20cf7cf186f35e3fceecbd2b55255a5f4203d83d
mqole <anactualpanacea@gmail.com>
2025-11-02 23:16:43 +1100

    MISSED ONE

--- Content.Shared/Strip/SharedStrippableSystem.cs
+++ Content.Shared/Strip/SharedStrippableSystem.cs
@@ -222,9 +222,7 @@ public abstract class SharedStrippableSystem : EntitySystem
             BreakOnDamage = true,
             BreakOnMove = true,
             NeedHand = true,
-            DuplicateCondition = stealth // imp add stealth
-                ? DuplicateConditions.None // imp operator: add none if null
-                : DuplicateConditions.SameTool
+            DuplicateCondition = stealth ? DuplicateConditions.None : DuplicateConditions.SameTool // block duplicates if using the thieving gloves : don't if not
         };
 
         _doAfterSystem.TryStartDoAfter(doAfterArgs);
@@ -328,9 +326,7 @@ public abstract class SharedStrippableSystem : EntitySystem
             BreakOnMove = true,
             NeedHand = true,
             BreakOnHandChange = false, // Allow simultaneously removing multiple items.
-            DuplicateCondition = stealth // imp add stealth
-                ? DuplicateConditions.None // imp operator: add none if null
-                : DuplicateConditions.SameTool
+            DuplicateCondition = stealth ? DuplicateConditions.None : DuplicateConditions.SameTool // block duplicates if using the thieving gloves : don't if not
         };
 
         _doAfterSystem.TryStartDoAfter(doAfterArgs);
@@ -433,9 +429,7 @@ public abstract class SharedStrippableSystem : EntitySystem
             BreakOnDamage = true,
             BreakOnMove = true,
             NeedHand = true,
-            DuplicateCondition = stealth // imp add stealth
-                ? DuplicateConditions.None // imp operator: add none if null
-                : DuplicateConditions.SameTool
+            DuplicateCondition = stealth ? DuplicateConditions.None : DuplicateConditions.SameTool // block duplicates if using the thieving gloves : don't if not
         };
 
         _doAfterSystem.TryStartDoAfter(doAfterArgs);
@@ -546,9 +540,7 @@ public abstract class SharedStrippableSystem : EntitySystem
             BreakOnMove = true,
             NeedHand = true,
             BreakOnHandChange = false, // Allow simultaneously removing multiple items.
-            DuplicateCondition = stealth // imp add stealth
-                ? DuplicateConditions.None // imp operator: add none if null
-                : DuplicateConditions.SameTool
+            DuplicateCondition = stealth ? DuplicateConditions.None : DuplicateConditions.SameTool // block duplicates if using the thieving gloves : don't if not
         };
 
         _doAfterSystem.TryStartDoAfter(doAfterArgs);
